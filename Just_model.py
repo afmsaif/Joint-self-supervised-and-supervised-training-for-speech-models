df4b9b10c559c5df27e443fe69cac39ed35f242f
afmsaif <32754388+afmsaif@users.noreply.github.com>
2023-10-23 16:20:32 -0400

    Update Just_model.py

--- Just_model.py
+++ Just_model.py
@@ -200,7 +200,7 @@ def data_processing(data, data_type="train"):
         spectrograms.append(spec)
         label = torch.Tensor(sentencepiece_transform.text_to_int(utterance))
         labels.append(label)
-        input_lengths.append(spec.shape[0]//2)
+        input_lengths.append(spec.shape[0])
         label_lengths.append(len(label))
 
     spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
